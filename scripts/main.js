b017bbf9cdb528bd72b7f6b95c7371a75a8dd8a3
Kasia <katarzyna-godlewska1@wp.pl>
2020-07-31 21:25:01 +0200

    Added module to show and hide containers

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 var buttonPlay = document.querySelector(".button-play");
-buttonPlay.addEventListener("click", function (e) { });
+buttonPlay.addEventListener("click", function (e) {
+    document
+        .querySelector(".game-container")
+        .classList.replace("game-container--hidden", "game-container--show");
+    document
+        .querySelector(".start-container")
+        .classList.replace("start-container--show", "start-container--hidden");
+});
 //# sourceMappingURL=main.js.map
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,3 +1,10 @@
 const buttonPlay = document.querySelector(".button-play");
 
-buttonPlay.addEventListener("click", (e) => {});
+buttonPlay.addEventListener("click", (e) => {
+  document
+    .querySelector(".game-container")
+    .classList.replace("game-container--hidden", "game-container--show");
+  document
+    .querySelector(".start-container")
+    .classList.replace("start-container--show", "start-container--hidden");
+});
